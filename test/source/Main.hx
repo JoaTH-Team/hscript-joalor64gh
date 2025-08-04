c52504d726877859f1b22003808e8a1ed7b64f19
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2025-08-04 08:12:21 +0700

    Update Main.hx

--- test/source/Main.hx
+++ test/source/Main.hx
@@ -5,7 +5,7 @@ class Main {
     
     static function main() {
         script = new HScript("test/executeMe.txt", true);
-        script.initConfig(); // Init all default Haxe classes
+        script.initConfig(); // Init some default Haxe classes
         script.set("cool", 12345);
         script.call("onRun", []);
     }
